b510209d6dcd5e0062755462a1cbe98d7ee057ae
Obed Ramirez Ortigoza <ObedRamirezOrtigoza@hotmail.com>
2024-10-30 00:37:25 +0000

    2do commit correcion de errores

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel
+from typing import Optional
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from settings import settings  # Import your settings module
+from settings import settings  
+import json
+
 
 app = FastAPI()
 
-# Use the correct attribute name
+class Stock(BaseModel):
+    symbol: str
+    stockname: str
+    lastsale: str
+    country: str
+    ipoyear: Optional[int] = None
+
+    
+with open('stocks.json', 'r') as f:
+    stocks = json.load(f)['stocks']
+
+
+
 engine = create_engine(settings.sqlalchemy_database_url)  # Use sqlalchemy_database_url here
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
+
+@app.get('/stock/{stock_symbol}', status_code=200)
+def get_stock(stock_symbol: str) -> Stock:
+    stock = [stock for stock in stocks if stock['symbol'] == stock_symbol]
+    if len(stock) == 0:
+        raise HTTPException(
+            status_code=404, detail=f"No stock {stock_symbol} found."
+         )
+
+    return stock[0]
+
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI application!"}
+
+
 
 def get_db():
     db = SessionLocal()
--- test_api.py
+++ test_api.py
@@ -1,5 +1,9 @@
 import requests
 
-response = requests.get('http://127.0.0.1:8000/stock/AAL')
+#response = requests.get('http://127.0.0.1:8000')
+#print(response.json())
 
-print(response.json())
+import requests
+
+print(requests.get('http://127.0.0.1:8000/stock/AMZN').json())
+#{'symbol': 'AMZN', 'stockname': 'Amazon.com Inc. Common Stock', 'lastsale': '$146.74', 'country': 'United States', 'ipoyear': 1997}
